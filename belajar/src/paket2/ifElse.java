3d3070f311e8c0bf78738717d32d04d1ea6a4ae7
Rolegian <mail.alfren@gmail.com>
2022-11-24 04:17:39 +0700

    menambahkan file ifElse/kondisional

---
+++ belajar/src/paket2/ifElse.java
@@ -0,0 +1,15 @@
+package paket2;
+
+public class ifElse {
+    public static void main(String[] args){
+        int x =10;
+        // if(yang ingin diuji)
+        if(x>10){
+            System.out.print("x lebih besar dari 10");
+        } else if(x == 10){
+            System.out.print("X sama dengan 10");
+        } else {
+            System.out.print("X kurang dari 10");
+        }
+    }
+}

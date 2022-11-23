10cae1bb889b96b418df4d411f884a68e3463a90
Rolegian <mail.alfren@gmail.com>
2022-11-24 04:54:37 +0700

    menambahkan file pengulangan

---
+++ belajar/src/paket2/pengulangan.java
@@ -0,0 +1,25 @@
+package paket2;
+
+public class pengulangan {
+    public static void main(String[] args){
+        // for
+        for(int i=0; i<=5; i++){
+            System.out.println(i);
+        }
+
+        //while
+        int i=0;
+        while (i<=5){
+            System.out.println("hasilnya adalah: "+ i);
+            i++;
+        }
+
+
+        // do while
+//        int i=6;
+//        do{
+//            System.out.println("Hasilnya"+ i);
+//            i++;
+//        }while(x<5);
+    }
+}

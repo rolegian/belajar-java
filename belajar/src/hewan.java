0374ec1316c8cc5e011b4836e718717977750c5f
Rolegian <mail.alfren@gmail.com>
2022-11-24 04:01:18 +0700

    memodifikasi file

---
+++ belajar/src/hewan.java
@@ -0,0 +1,11 @@
+public class hewan {
+    public static void main(String[] args) {
+
+        //tipeData namaVariabel =nilai
+        //tipeData namaVariabel;
+        int x= 10;
+        String text = "ini contoh string";
+        System.out.println(x);
+        System.out.println(text);
+    }
+}
